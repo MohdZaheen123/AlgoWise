e8d099b4d0032567f7c2b3c152fc5205c5a3aeee
Mohamed zaheen <mohamedzaheen986@gmail.com>
2024-01-02 08:08:47 +0530

    dsa sidebar bug fix

--- src/components/CommingSoon.jsx
+++ src/components/CommingSoon.jsx
@@ -4,7 +4,7 @@ import SparklesCore from "@/components/SparklesCore";
 
 export  function CommingSoon() {
   return (
-    <div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+    <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
       <div className="w-full absolute inset-0 h-screen">
         <SparklesCore
           id="tsparticlesfullpage"
