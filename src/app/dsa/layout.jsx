f22d523846e5900c2e90a1f7c7c834478fa094ae
Mohamed zaheen <mohamedzaheen986@gmail.com>
2024-01-02 08:00:05 +0530

    dsa page updated

--- src/app/dsa/layout.jsx
+++ src/app/dsa/layout.jsx
@@ -1,11 +1,21 @@
+'use client'
+import { useState } from 'react';
+import { cn } from "@/lib/utils";
+import { AlignJustify,X } from 'lucide-react';
 import SideNav from '@/components/SideNav';
  
 export default function Layout({ children }) {
+
+  const [showSideNav, setShowSideNav] = useState(false);
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-60">
+    <div className="flex  h-screen  flex-row md:overflow-hidden">
+      <div className={cn(" flex-none md:w-60 hidden md:block",
+      showSideNav ? 'block' : 'hidden'
+      
+      )}>
         <SideNav />
       </div>
+      <button onClick={()=>{ setShowSideNav(!showSideNav)}} className='text-white w-10 h-10 bg-black md:hidden absolute top-5 left-7 z-30'>{showSideNav?<X/>:<AlignJustify />}</button>
       <div className="flex-grow md:overflow-y-auto ">{children}</div>
     </div>
   );
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function SideNav() {
   return (
     <aside className="w-full p-6 sm:w-60 dark:bg-gray-900 dark:text-gray-100 text-white">
-        <p className='mt-5 mb-2'>Welcome to DSA prep</p>
+        <p className='mt-10 mb-2'>Welcome to DSA prep</p>
         <Link className='flex items-center mb-8 text-md text-red-500' href='/blogs'><MousePointer2 className='h-4'/>Back</Link>
 	<nav className="space-y-8 text-sm">
 		<div className="space-y-2">
