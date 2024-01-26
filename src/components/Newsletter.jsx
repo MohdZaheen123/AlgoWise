198053a1b75b31964fd8573fab091f15aa917227
Mohamed zaheen <mohamedzaheen986@gmail.com>
2024-01-26 16:50:20 +0530

    UI update

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -6,11 +6,11 @@ export default function Newsletter() {
         
     <div className='w-screen  mx-auto h-[44rem]  '>
       <Checkout />
-      <div className="h-[36rem]">
+      <div className="h-[33rem]">
       <div data-title="Newsletter Subscription" data-url="31oogmqr-newsletter-subscription" data-domain="forms" data-full-page="false" className='visme_d mx-auto h-[50rem] w-[100%] mb-24' data-form-id="13228"></div>
       {/* <iframe aria-label="News letter" className='mx-auto h-[50rem] w-[100%] mb-24' src="https://forms.visme.co/formsPlayer/90p7rqqm-expert-blog-subscription" frameborder="0"></iframe> */}
 
-      <div className="md:h-60 h-10 top-[-29rem] left-[16rem] w-40 hidden md:block  md:w-64 bg-black z-20 relative md:top-[-5.5rem] md:left-[54rem]"></div>
+      <div className="md:h-60 h-10 top-[-20rem] left-[16rem] w-40 hidden md:block  md:w-64 bg-black z-20 relative md:top-[-4rem] md:left-[54rem]"></div>
       </div>
     </div>
 
